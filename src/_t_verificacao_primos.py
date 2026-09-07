0904fa94456a6e10533a3b488cb765bacffa2dc7
Oliver Melo <contato_limaverde@protonmail.com>
2026-09-07 00:48:01 -0300

    removendo testes de funcoes removidas e corrigindo imports

--- src/_t_verificacao_primos.py
+++ src/_t_verificacao_primos.py
@@ -1,21 +1,11 @@
-from verificacao_primos import VerificaPrimos
+from algoritmos_fundamentais import is_prime 
 
 """Testes unitários para a classe VerificaPrimos"""
 
 print("Método simples")
-print(VerificaPrimos.simples(2))  # True
-print(VerificaPrimos.simples(3))  # True
-print(VerificaPrimos.simples(4))  # False
-print(VerificaPrimos.simples(5))  # True
-print(VerificaPrimos.simples(16))  # False
-print(VerificaPrimos.simples(17))  # True
-
-print("\nMétodo Miller-Rabin")
-k = 40  # Número de rodadas de teste
-print(VerificaPrimos.miller_rabin(7919, k))  # True
-print(VerificaPrimos.miller_rabin(6389, k))  # True
-print(VerificaPrimos.miller_rabin(97686, k))  # False
-print(VerificaPrimos.miller_rabin(293, k))  # True
-print(VerificaPrimos.miller_rabin(7855, k))  # False
-print(VerificaPrimos.miller_rabin(17, k))  # True
-
+print(is_prime(2))  # True
+print(is_prime(3))  # True
+print(is_prime(4))  # False
+print(is_prime(5))  # True
+print(is_prime(16))  # False
+print(is_prime(17))  # True
