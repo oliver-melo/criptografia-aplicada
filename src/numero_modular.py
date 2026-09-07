cd816c78a76bfaa7a60ab6b665d871b08330c623
Oliver Melo <contato_limaverde@protonmail.com>
2026-09-07 00:52:02 -0300

    correcao de import ciclico: novo arquivo algoritmos_numero_modular.py, algoritmos.py virou algoritmos_fundamentais.py, aritmetica_modular.py virou numero_modular.py

--- src/numero_modular.py
+++ src/numero_modular.py
@@ -1,4 +1,4 @@
-from algoritmos import euclides_estendido
+from algoritmos_fundamentais import euclides_estendido
 
 class NumeroModular:
     """Classe para representar números em espaços modulares."""
